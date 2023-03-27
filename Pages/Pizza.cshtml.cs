d721748685e67ff9f7e64ac4e507b192db1a4166
Stefanus Terang <79499530+TerangStefanus@users.noreply.github.com>
2023-03-27 14:08:36 +0700

    Add project files.

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaApp.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
